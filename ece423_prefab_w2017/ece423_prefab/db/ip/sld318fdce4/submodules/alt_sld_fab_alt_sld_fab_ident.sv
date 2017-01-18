bc59a3966d67c7c3d900dc7c5a50bcf6a736bf0e
Ghanan Gowripalan <ggowripa@uwaterloo.ca>
2017-01-17 20:51:32 -0500

    Updated the .v to output the global reset for the lpddr

--- ece423_prefab_w2017/ece423_prefab/db/ip/sld318fdce4/submodules/alt_sld_fab_alt_sld_fab_ident.sv
+++ ece423_prefab_w2017/ece423_prefab/db/ip/sld318fdce4/submodules/alt_sld_fab_alt_sld_fab_ident.sv
@@ -20,7 +20,7 @@
 // Altera Identification hub
 //
 // Parameters
-//   DESIGN_HASH   : c79d56a04115ac30478e
+//   DESIGN_HASH   : 66ef470d216c9dc8cf4a
 //   COUNT         : 1
 //   ROM_WIDTHS    : 4
 //   LATENCIES     : 0
@@ -39,7 +39,7 @@ module alt_sld_fab_alt_sld_fab_ident
 );
 
 
-wire [127:0] data_0 = { mixed, 12'h0, 32'h0, 80'hc79d56a04115ac30478e };
+wire [127:0] data_0 = { mixed, 12'h0, 32'h0, 80'h66ef470d216c9dc8cf4a };
 
 reg [3:0] result_0;
 always @(address_0 or data_0) begin
