d0aa155862b0bdfd49e66b9d1af5fc05e2886e67
Ghanan Gowripalan <ghanan94@gmail.com>
2017-02-27 20:22:03 -0500

    Use array of std_logic_vector to split up input/output test buffers in streaming int tb

--- idct/idct_2d_streaming_interface_tb.vhd
+++ idct/idct_2d_streaming_interface_tb.vhd
@@ -4,51 +4,61 @@ use ieee.numeric_std.all;
 
 entity idct_2D_streaming_interface_tb is
   generic (
-    PERIOD : real := 20.0;
-    TEST_DATA_INPUT_BYTE_COUNT : integer := 256;
-    TEST_DATA_INPUT : std_logic_vector(2047 downto 0) := X"6400_0000_0000_0000_0000_0000_0000_0000"
-        & X"0000_6400_0000_0000_0000_0000_0000_0000"
-        & X"0000_0000_6400_0000_0000_0000_0000_0000"
-        & X"0000_0000_0000_6400_0000_0000_0000_0000"
-        & X"0000_0000_0000_0000_6400_0000_0000_0000"
-        & X"0000_0000_0000_0000_0000_6400_0000_0000"
-        & X"0000_0000_0000_0000_0000_0000_6400_0000"
-        & X"0000_0000_0000_0000_0000_0000_0000_6400"
-
-        & X"D804_0000_F6FF_0000_0000_0000_0000_0000"
-        & X"E8FF_F4FF_0000_0000_0000_0000_0000_0000"
-        & X"F2FF_F3FF_0000_0000_0000_0000_0000_0000"
-        & X"0000_0000_0000_0000_0000_0000_0000_0000"
-        & X"0000_0000_0000_0000_0000_0000_0000_0000"
-        & X"0000_0000_0000_0000_0000_0000_0000_0000"
-        & X"0000_0000_0000_0000_0000_0000_0000_0000"
-        & X"0000_0000_0000_0000_0000_0000_0000_0000";
-
-    TEST_DATA_OUTPUT_BYTE_COUNT : integer := 128;
-    TEST_DATA_OUTPUT : std_logic_vector(1023 downto 0) := X"64_00_00_00_00_00_00_00"
-        & X"00_64_00_00_00_00_00_00"
-        & X"00_00_64_00_00_00_00_00"
-        & X"00_00_00_64_00_00_00_00"
-        & X"00_00_00_00_64_00_00_00"
-        & X"00_00_00_00_00_64_00_00"
-        & X"00_00_00_00_00_00_64_00"
-        & X"00_00_00_00_00_00_00_64"
-
-        & X"8D_8F_92_95_97_99_99_99"
-        & X"91_93_95_97_99_99_99_99"
-        & X"98_99_9A_9B_9B_9B_99_98"
-        & X"9D_9E_9E_9F_9E_9C_9A_98"
-        & X"A0_A0_A1_A0_9F_9D_9A_99"
-        & X"A0_A0_A1_A0_9F_9D_9B_9A"
-        & X"9D_9E_9F_9F_9F_9E_9C_9B"
-        & X"9B_9C_9E_9E_9F_9E_9C_9B"
+    PERIOD : real := 20.0
   );
 end entity idct_2D_streaming_interface_tb;
 
 architecture main of idct_2D_streaming_interface_tb is
   constant CLK_PERIOD : time := PERIOD * 1 ns;
-  constant TEST_DATA_INPUT_LENGTH : integer := TEST_DATA_INPUT'length;
-  constant TEST_DATA_OUTPUT_LENGTH : integer := TEST_DATA_OUTPUT'length;
+
+  constant NUM_TESTS : integer := 2;
+  constant WORD_LENGTH : integer := 32;
+
+  constant TEST_DATA_INPUT_LENGTH : integer := 1024;
+  constant TEST_DATA_INPUT_WORDS : integer := (TEST_DATA_INPUT_LENGTH / WORD_LENGTH);
+  type input_data is array(0 to 1) of std_logic_vector((TEST_DATA_INPUT_LENGTH - 1) downto 0);
+  constant TEST_DATA_INPUT : input_data := (
+    X"6400_0000_0000_0000_0000_0000_0000_0000"
+    & X"0000_6400_0000_0000_0000_0000_0000_0000"
+    & X"0000_0000_6400_0000_0000_0000_0000_0000"
+    & X"0000_0000_0000_6400_0000_0000_0000_0000"
+    & X"0000_0000_0000_0000_6400_0000_0000_0000"
+    & X"0000_0000_0000_0000_0000_6400_0000_0000"
+    & X"0000_0000_0000_0000_0000_0000_6400_0000"
+    & X"0000_0000_0000_0000_0000_0000_0000_6400"
+
+    , X"D804_0000_F6FF_0000_0000_0000_0000_0000"
+    & X"E8FF_F4FF_0000_0000_0000_0000_0000_0000"
+    & X"F2FF_F3FF_0000_0000_0000_0000_0000_0000"
+    & X"0000_0000_0000_0000_0000_0000_0000_0000"
+    & X"0000_0000_0000_0000_0000_0000_0000_0000"
+    & X"0000_0000_0000_0000_0000_0000_0000_0000"
+    & X"0000_0000_0000_0000_0000_0000_0000_0000"
+    & X"0000_0000_0000_0000_0000_0000_0000_0000"
+  );
+
+  constant TEST_DATA_OUTPUT_LENGTH : integer := 512;
+  constant TEST_DATA_OUTPUT_WORDS : integer := (TEST_DATA_OUTPUT_LENGTH / WORD_LENGTH);
+  type output_data is array(0 to 1) of std_logic_vector((TEST_DATA_OUTPUT_LENGTH - 1) downto 0);
+  constant TEST_DATA_OUTPUT : output_data := (
+    X"64_00_00_00_00_00_00_00"
+    & X"00_64_00_00_00_00_00_00"
+    & X"00_00_64_00_00_00_00_00"
+    & X"00_00_00_64_00_00_00_00"
+    & X"00_00_00_00_64_00_00_00"
+    & X"00_00_00_00_00_64_00_00"
+    & X"00_00_00_00_00_00_64_00"
+    & X"00_00_00_00_00_00_00_64"
+
+    , X"8D_8F_92_95_97_99_99_99"
+    & X"91_93_95_97_99_99_99_99"
+    & X"98_99_9A_9B_9B_9B_99_98"
+    & X"9D_9E_9E_9F_9E_9C_9A_98"
+    & X"A0_A0_A1_A0_9F_9D_9A_99"
+    & X"A0_A0_A1_A0_9F_9D_9B_9A"
+    & X"9D_9E_9F_9F_9F_9E_9C_9B"
+    & X"9B_9C_9E_9E_9F_9E_9C_9B"
+  );
 
   signal clk : std_logic := '0';
   signal reset : std_logic := '0';
@@ -103,12 +113,16 @@ begin
   process
   begin
     dest_valid <= '1';
+    
+    for test_num in 0 to (NUM_TESTS - 1) loop
+      report("Input Test #" & integer'image(test_num));
 
-    for i in 0 to ((TEST_DATA_INPUT_BYTE_COUNT / 4) - 1) loop
-        dest_data <= TEST_DATA_INPUT((TEST_DATA_INPUT_LENGTH - (i * 32) - 1) 
-            downto (TEST_DATA_INPUT_LENGTH - (i * 32) - 32));
+      for i in 0 to (TEST_DATA_INPUT_WORDS - 1) loop
+        dest_data <= TEST_DATA_INPUT(test_num)((TEST_DATA_INPUT_LENGTH - (i * WORD_LENGTH) - 1) 
+            downto (TEST_DATA_INPUT_LENGTH - (i * WORD_LENGTH) - WORD_LENGTH));
 
         wait until rising_edge(clk) and dest_ready = '1';
+      end loop;
     end loop;
 
     report("Done Inputs");
@@ -122,14 +136,18 @@ begin
   begin
     src_ready <= '1';
 
-    for i in 0 to ((TEST_DATA_OUTPUT_BYTE_COUNT / 4) - 1) loop
-        expected_src_data <= TEST_DATA_OUTPUT((TEST_DATA_OUTPUT_LENGTH - (i * 32) - 1) 
-            downto (TEST_DATA_OUTPUT_LENGTH - (i * 32) - 32));
+    for test_num in 0 to (NUM_TESTS - 1) loop
+      report("Output Test #" & integer'image(test_num));
 
-        wait until rising_edge(clk) and src_valid = '1';
-        assert(src_data = expected_src_data) report("ASSERT FAILED: Got " 
-                & integer'image(to_integer(unsigned(src_data))) & " when expected was " 
-                & integer'image(to_integer(unsigned(expected_src_data))));
+      for i in 0 to (TEST_DATA_OUTPUT_WORDS - 1) loop
+          expected_src_data <= TEST_DATA_OUTPUT(test_num)((TEST_DATA_OUTPUT_LENGTH - (i * WORD_LENGTH) - 1) 
+              downto (TEST_DATA_OUTPUT_LENGTH - (i * WORD_LENGTH) - WORD_LENGTH));
+
+          wait until rising_edge(clk) and src_valid = '1';
+          assert(src_data = expected_src_data) report("ASSERT FAILED: Got " 
+                  & integer'image(to_integer(unsigned(src_data))) & " when expected was " 
+                  & integer'image(to_integer(unsigned(expected_src_data))));
+      end loop;
     end loop;
 
     report("Done Outputs");
